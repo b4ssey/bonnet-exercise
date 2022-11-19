f8941e7eed52d59b429672c969b13b63bd968bfb
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-11-19 22:55:26 +0000

    reworked task2

--- task2.js
+++ task2.js
@@ -1,53 +1,45 @@
 function getStartTime(schedules, duration) {
   let toMinutes = (s) => {
-    s.split(":").reduce((h, m) => parseInt(h) * 60 + parseInt(m));
+    return s.split(":").reduce((h, m) => parseInt(h) * 60 + parseInt(m));
   };
-
-  let toTime = (t) => {
-    let h = Math.floor(t / 60);
-    let m = t % 60;
-
-    return `${h}:${m}`;
-  };
-
+  function pad(num) {
+    var s = num.toString();
+    if (s.length == 1) {
+      s = "0" + s;
+    }
+    return s;
+  }
   let sortedSchedules = schedules
     .reduce((p, n) => p.concat(n))
     .map((a) => [toMinutes(a[0]), toMinutes(a[1])])
     .sort((a, b) => a[0] - b[0]);
 
   let presentSchedule = sortedSchedules[0];
-  for (let schedule of sortedSchedules) {
+  let rry = [];
+  rry.push(presentSchedule);
+  if (duration + 540 <= presentSchedule[0])
+    return pad(Math.floor(540 / 60)) + ":" + pad(540 % 60);
+  for (let i = 0; i < sortedSchedules.length; i++) {
     let [_, presentScheduleEnd] = presentSchedule;
-    let [futureScheduleStart, futureScheduleEnd] = schedule;
+    let [futureScheduleStart, futureScheduleEnd] = sortedSchedules[i];
     if (presentScheduleEnd >= futureScheduleStart) {
       presentSchedule[1] = Math.max(presentScheduleEnd, futureScheduleEnd);
     } else {
-      presentSchedule = schedule;
-      if (presentScheduleEnd + duration <= futureScheduleStart)
-        return toTime(presentScheduleEnd);
+      presentSchedule = sortedSchedules[i];
+      rry.push(presentSchedule);
     }
   }
+  for (let y = 0; y < rry.length - 1; y++) {
+    if (rry[y][1] + duration <= rry[y + 1][0]) {
+      return pad(Math.floor(rry[y][1] / 60)) + ":" + pad(rry[y][1] % 60);
+    }
+  }
+  if (duration + rry[rry.length - 1][1] <= 1140)
+    return (
+      pad(Math.floor(rry[rry.length - 1][1] / 60)) +
+      ":" +
+      pad(rry[rry.length - 1][1] % 60)
+    );
+
   return null;
 }
-
-getStartTime(
-  [
-    [
-      ["09:00", "11:30"],
-      ["13:30", "16:00"],
-      ["16:00", "17:30"],
-      ["17:45", "19:00"],
-    ],
-    [
-      ["09:15", "12:00"],
-      ["14:00", "16:30"],
-      ["17:00", "17:30"],
-    ],
-    [
-      ["11:30", "12:15"],
-      ["15:00", "16:30"],
-      ["17:45", "19:00"],
-    ],
-  ],
-  30
-);
