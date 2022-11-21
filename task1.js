790705d546cf920256c90f8c8f56105b6337da5b
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-11-21 10:29:23 +0000

    added example for task1

--- task1.js
+++ task1.js
@@ -24,3 +24,22 @@ function defaultArguments(func, params) {
   }
   return results;
 }
+
+//example
+function add(a, b) {
+  return a + b;
+}
+
+const add2 = defaultArguments(add, { b: 9 });
+console.assert(add2(10) === 9);
+console.assert(add2(10, 7) === 17);
+console.assert(isNaN(add2()));
+
+const add3 = defaultArguments(add, { a: 3, b: 9 });
+console.assert(add3(10) === 13);
+console.assert(add3() === 5);
+console.assert(add3(undefined, 10) === 12);
+
+const add4 = defaultArguments(add, { c: 3 });
+console.assert(isNaN(add4(10)));
+console.assert(add4(10, 10) === 20);
