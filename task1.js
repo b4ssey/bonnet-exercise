d9bc502de563c452018b82e27a265722d749b1e4
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-11-21 10:30:03 +0000

    added example for task1

--- task1.js
+++ task1.js
@@ -31,15 +31,15 @@ function add(a, b) {
 }
 
 const add2 = defaultArguments(add, { b: 9 });
-console.assert(add2(10) === 9);
-console.assert(add2(10, 7) === 17);
-console.assert(isNaN(add2()));
+console.log(add2(10) === 19);
+console.log(add2(10, 7) === 17);
+console.log(isNaN(add2()));
 
-const add3 = defaultArguments(add, { a: 3, b: 9 });
-console.assert(add3(10) === 13);
-console.assert(add3() === 5);
-console.assert(add3(undefined, 10) === 12);
+const add3 = defaultArguments(add, { a: 2, b: 3 });
+console.log(add3(10) === 13);
+console.log(add3() === 5);
+console.log(add3(null, 10) === 12);
 
 const add4 = defaultArguments(add, { c: 3 });
-console.assert(isNaN(add4(10)));
-console.assert(add4(10, 10) === 20);
+console.log(isNaN(add4(10)));
+console.log(add4(10, 10) === 20);
